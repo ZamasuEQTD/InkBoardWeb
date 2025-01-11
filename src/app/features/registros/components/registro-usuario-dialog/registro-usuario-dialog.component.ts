72ae07d65de538716f753f2f9c7d0df1581a7ef8
Franco <ff>
2025-01-11 19:40:10 -0300

    registro como componente individual

--- src/app/features/registros/components/registro-usuario-dialog/registro-usuario-dialog.component.ts
+++ src/app/features/registros/components/registro-usuario-dialog/registro-usuario-dialog.component.ts
@@ -2,12 +2,13 @@ import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { DialogHeaderComponent } from "../../../shared/components/dialog-header/dialog-header.component";
 import { DialogRef } from '@angular/cdk/dialog';
+import { RegistroComponent } from "../registro/registro.component";
 
 @Component({
   selector: 'app-registro-usuario-dialog',
   templateUrl: './registro-usuario-dialog.component.html',
   styleUrl: './registro-usuario-dialog.component.css',
-  imports: [],
+  imports: [RegistroComponent],
 })
 export class RegistroUsuarioDialogComponent {
   dialogRef = inject(DialogRef);
