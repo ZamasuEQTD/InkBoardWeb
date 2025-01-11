3ff0b201826da513a4b89125df9ea36064650670
Franco <ff>
2025-01-11 19:42:47 -0300

    banear usuario dialog agregado

---
+++ src/app/features/moderacion/banear-usuario-dialog/banear-usuario-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banear-usuario-dialog',
+  imports: [],
+  templateUrl: './banear-usuario-dialog.component.html',
+  styleUrl: './banear-usuario-dialog.component.css',
+})
+export class BanearUsuarioDialogComponent { }
