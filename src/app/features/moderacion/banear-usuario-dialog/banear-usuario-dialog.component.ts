57a7e26e1d0664fb6259fb45eed375699af4d0b2
Franco <ff>
2025-01-11 22:07:41 -0300

    form agregado

--- src/app/features/moderacion/banear-usuario-dialog/banear-usuario-dialog.component.ts
+++ src/app/features/moderacion/banear-usuario-dialog/banear-usuario-dialog.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DialogComponent } from "../../shared/components/dialog/dialog.component";
+import { InputLabeledComponent } from "../../shared/components/input-labeled/input-labeled.component";
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-banear-usuario-dialog',
-  imports: [DialogComponent],
+  imports: [DialogComponent, InputLabeledComponent],
   templateUrl: './banear-usuario-dialog.component.html',
   styleUrl: './banear-usuario-dialog.component.css',
 })
-export class BanearUsuarioDialogComponent { }
+export class BanearUsuarioDialogComponent {
+  static razones : RazonDeBaneo[] = [
+    {
+      id: 0,
+      razon: "Contenido Inapropiado"
+    }
+  ]
+
+  static duracions : DuracionDeBaneo [] = []
+
+  private fb : FormBuilder  = inject(FormBuilder);
+
+  form = this.fb.group({
+    razon: this.fb.control<RazonDeBaneo | undefined>(undefined),
+    duracion: this.fb.control<DuracionDeBaneo | undefined>(undefined),
+    mensaje: this.fb.control<string>("")
+  })
+}
+
+
+interface RazonDeBaneo {
+  id:number
+  razon: string
+}
+
+interface DuracionDeBaneo {
+  id:number
+  duracion: string
+}
