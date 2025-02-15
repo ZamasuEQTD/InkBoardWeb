dde08d288d591eff7a08a4bb354acf2f30fd7508
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-02-14 22:02:17 -0300

    mejorada la presentación del historial de comentarios y optimizada la estructura del componente media-box para soportar dimensiones ajustables

--- src/app/features/shared/components/media-box/media-box.component.ts
+++ src/app/features/shared/components/media-box/media-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { YoutubePlayerComponent } from "../youtube-player/youtube-player.component";
 import { CommonModule } from '@angular/common';
 import { Media } from '../../../core/models/media.interface';
@@ -9,17 +9,21 @@ import { Media } from '../../../core/models/media.interface';
   templateUrl: './media-box.component.html',
   styleUrl: './media-box.component.css',
 })
-export class MediaBoxComponent implements OnInit {
-  
+export class MediaBoxComponent implements OnInit , OnDestroy{
   static dimensionables :string [] = ['youtube', 'image', 'gif' ,'video']
+
+  @ViewChild("video") video? : ElementRef<HTMLVideoElement>;
 
   @Input() media!:Media;
 
   isDimensionable !:boolean;
+  @Input() dimensionableSize?: {maxWidth? : string,maxHeight? : string} 
 
   ngOnInit(): void {
     this.isDimensionable = MediaBoxComponent.dimensionables.includes(this.media.provider);
   }
 
-  @Input() dimensionableSize?: {maxWidth? : string,maxHeight? : string} 
+  ngOnDestroy(): void {
+  }
+
 }
